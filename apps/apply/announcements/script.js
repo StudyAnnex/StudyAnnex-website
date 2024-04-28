6b96af8e4aadb99fdbe730584994358e8edd9cda
Cwenga <145836871+StudyAnnex@users.noreply.github.com>
2024-04-28 23:20:09 +0200

    Add files via upload

--- apps/apply/announcements/script.js
+++ apps/apply/announcements/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.anouncement').forEach((value)=>{
+/*document.querySelectorAll('.anouncement').forEach((value)=>{
   let announceNum = value.dataset.announce;
   const descriptionElement = document.querySelector(`.js-description-${announceNum}`);
   if(descriptionElement.style.display === "none"){
@@ -6,5 +6,17 @@ document.querySelectorAll('.anouncement').forEach((value)=>{
   }
   else{
     descriptionElement.style.display = "none";
+  }
+});*/
+
+const announcement = document.querySelector(".announcement-1")
+announcement.addEventListener('click', ()=>{
+  
+  const description = document.querySelector('.description-1');
+
+  if(description.style.display==="block"){
+    description.style.display = "none";
+  } else {
+    description.style.display = "block";
   }
 });
